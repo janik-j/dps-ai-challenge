6e2c792cc4958f81ae712e71c084ddf4f9487475
Janik Alexander Jehkul <janik.jehkul@tum.de>
2024-07-02 10:47:46 +0200

    Add visualise_data function for plotting traffic accidents over time, saved Alkoholunfälle, Fluchtunfälle, Verkehrsunfälle in media folder

--- src/data/data_utils.py
+++ src/data/data_utils.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-from typing import Tuple, List
-from datetime import datetime
-
+import os
+from typing import List
 
 def load_and_filter_data(filepath : str, start_year : int = 2000, end_year : int = 2024) -> pd.DataFrame:
     """
@@ -25,3 +23,44 @@ def load_and_filter_data(filepath : str, start_year : int = 2000, end_year : int
     data = data[(data['datetime'].dt.year >= start_year) & (data['datetime'].dt.year <= end_year)]
     
     return data
+
+def visualise_data(df: pd.DataFrame, title: str = 'Total amount of traffic accidents over time') -> List[str]:
+    """
+    Visualise the data by plotting the total amount of traffic accidents over time.
+
+    Parameters:
+    df (pd.DataFrame): The filtered data.
+    title (str): The title of the plot.
+
+    Returns: A list of file paths to the saved plots.
+    """
+
+    categories = ['Alkoholunfälle', 'Fluchtunfälle', 'Verkehrsunfälle']
+    df = df[df['AUSPRAEGUNG'] == 'insgesamt']
+    pd.to_datetime(df['datetime'])
+    df.set_index('datetime', inplace=True)
+
+    created_files = []  
+
+    for category in categories:
+        df_category = df[df['MONATSZAHL'] == category]
+        
+        # Splitting the data
+        train = df_category[:'2020']
+        test = df_category['2021':]
+        
+        plt.figure(figsize=(15, 5))
+        plt.plot(train.index, train['WERT'], '.', label='Train Set', color='blue')
+        plt.plot(test.index, test['WERT'], '.', label='Test Set', color='red')
+        if not train.empty and not test.empty:
+            split_date = pd.Timestamp('2021-01-01')
+            plt.axvline(x=split_date, color='black', linestyle='--', label='Train/Test Split')
+        plt.title(f'{title} - {category}')
+        plt.legend()
+        
+        file_path = os.path.join("media", f'{category}.png')
+        plt.savefig(file_path)
+        plt.close()
+        created_files.append(file_path)  
+    
+    return created_files
--- src/main.py
+++ src/main.py
@@ -3,14 +3,15 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-from data.data_utils import load_and_filter_data
+from data.data_utils import load_and_filter_data, visualise_data
 
 def main():
     # Path to the raw data file
     csv_filepath = os.path.join('data', 'monthly_traffic_accidents.csv')
     # Process and save data
-    df = load_and_filter_data(csv_filepath, 2000, 2020)
-    print(df.head())
-    
+    df = load_and_filter_data(csv_filepath, 2000, 2024)
+    print(visualise_data(df))
+
+
 if __name__ == '__main__':
     main()
