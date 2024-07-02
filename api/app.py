ab11d3f53f7a641f9d1737a108a463cd31023e49
Janik Alexander Jehkul <janik.jehkul@tum.de>
2024-07-02 16:57:29 +0200

    Fixed model_path in app.py

--- api/app.py
+++ api/app.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 
 # Load the model
-model_path = os.path.join(os.path.dirname(__file__), 'models', 'xgp_model.joblib')
+model_path = os.path.join(os.path.dirname(__file__), '..', 'checkpoints', 'xgp_model.joblib')
 model = joblib.load(model_path)
 
 @app.route('/predict', methods=['POST'])
